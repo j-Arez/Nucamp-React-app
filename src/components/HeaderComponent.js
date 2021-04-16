0f608468e352c74f805c6d651324240b5ee19117
Jonathan Almendarez <jonsaa13110@gmail.com>
2021-04-16 14:44:34 -0400

    Fixed logos and links

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -34,7 +34,7 @@ class Header extends Component{
                 </Jumbotron>
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png"
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/images/logo.png"
                         height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
