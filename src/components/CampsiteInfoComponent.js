2307c986be2128a86a7062a552b5de6ff6f5ed3f
Jonathan Almendarez <jonsaa13110@gmail.com>
2021-04-28 12:01:00 -0400

    Redux Action Created

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -27,9 +27,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -101,7 +100,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <>
@@ -123,7 +122,7 @@ function RenderComments({ comments }) {
                         );
                     })}
                 </div>
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
 
             </>
         );
@@ -148,7 +147,10 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
