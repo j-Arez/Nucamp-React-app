0fb711798054f81fedb319d156b79d98d9fd100b
Jonathan Almendarez <jonsaa13110@gmail.com>
2021-04-30 16:25:18 -0400

    Fetch from server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import {
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const maxLength = len => val => !val || (val.length <= len);
@@ -92,7 +93,7 @@ function RenderCampsite({ campsite }) {
     return (
         <div className="cold-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
